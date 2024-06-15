5844175d0caa33408315e95f0836e4b1bdb4152b
Cam McInnes <camtmcinnes@gmail.com>
2024-06-15 10:18:31 -0700

    Make Navbar with Animation
    Todo: move navbar up and move text to fill left

--- main/src/Pages/Home.js
+++ main/src/Pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../style/home.css';
 import Navbar from '../components/Navbar';
-import ResponsiveAppBar from '../components/Header';
+
 
 const Home = () => {
   return (
     <div>
-      <ResponsiveAppBar/>
+      <Navbar/>
     </div>
   );
 };
--- main/src/components/Navbar.js
+++ main/src/components/Navbar.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import './Nav.css';
 
-const Navbar = () => {
+import { useNavigate } from 'react-router-dom';
+
+const pages = ['About', 'Blog', 'Project'];
+
+function ResponsiveAppBar() {
+  const [menutoggle, setMenuToggle] = React.useState(null);
+  const navigate = useNavigate();
+
+  const OpenNavMenu = (event) => {
+    setMenuToggle(event.currentTarget);
+  };
+
+  const CloseNavMenu = () => {
+    setMenuToggle(null);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+    <div className='header'>
+       <div className='logo-nav'>
+        <a className='left-logo' href='/' onClick={() => {CloseNavMenu(); navigate('/')}}>McInnes</a>
+       </div>
+        <div className='right-navigations'>
 
-export default Navbar;
+            <div className={menutoggle? "nav-options active" : "nav-options"}>
+                <a onClick={()=>{CloseNavMenu();navigate('/')}}>Home</a>
+                <a onClick={()=>{CloseNavMenu();navigate('/about')}}>About</a>
+                <a onClick={()=>{CloseNavMenu();navigate('/projects')}}>Projects</a>
+                <a onClick={()=>{CloseNavMenu();navigate('/blogs')}}>Blogs</a>
+                <a onClick={()=>{CloseNavMenu();navigate('/contact')}}>Contact</a>
+            </div>
+        </div>
+    </div>
+
+  );
+}
+export default ResponsiveAppBar;
